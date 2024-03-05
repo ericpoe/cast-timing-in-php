e8e084a69600fe8d2b91ad5db6a4fc6e3d6e81be
Eric Poe <eric.poe@gmail.com>
2024-03-05 09:26:07 +0100

    Remove unused code
    Previously commented out

--- src/Repository/TimingRepository.php
+++ src/Repository/TimingRepository.php
@@ -38,29 +38,4 @@ class TimingRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
-
-//    /**
-//     * @return Timing[] Returns an array of Timing objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('t.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Timing
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
